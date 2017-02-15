b5ffe0a26f2ef24b48a011d60327fce581e644b5
Unknown <TruongVu.email@gmail.com>
2017-02-15 16:32:20 -0700

    Created and tested API to ensure basic functionality

--- SuperEffectivePokeAPI/SuperEffectivePokeAPI/ViewController.swift
+++ SuperEffectivePokeAPI/SuperEffectivePokeAPI/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2017 Truong Vu. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
 class ViewController: UIViewController {
@@ -20,9 +21,50 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var EffectivenessOutput: UILabel!
+    @IBOutlet weak var attackInput: UITextField!
     @IBAction func CheckButton(_ sender: Any) {
         
+        
+        
+        var url = "https://pokeapi.co/api/v2/type/" + attackInput.text! + "/";
+        print(url);
+        
+        callPokeAPI(url: url)
+        
     }
+    
+    func callPokeAPI(url:String) -> String
+    {
+        
+        let url = NSURL(string: url)!
+        let urlSess = URLSession.shared;
+        let semaphore = DispatchSemaphore.init(value: 0);
+        
+        let jsonQuery = urlSess.dataTask(with: url as URL, completionHandler: { data, response, error -> Void in
+            if (error != nil)
+            {
+                print(error!.localizedDescription)
+            }
+            
+            var err: NSError?
+            var jsonResult = (try! JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers)) as! NSDictionary
+            if (err != nil)
+            {
+                print("JSON Error \(err!.localizedDescription)")
+            }
+            
+            //self.EffectivenessOutput.text =
+                print(jsonResult["name"] as! String)
+            print("Hello");
+        })
+        
+        jsonQuery.resume();
+        
+        
+        return "Hello";
+        
+        }
 
 }
 
